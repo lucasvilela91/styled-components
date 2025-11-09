68899cfda72b423ff3089668357d5e74ccd87029
Lucas Freitas Vilela <152106763+lucasvilela91@users.noreply.github.com>
2025-11-09 19:29:19 -0300

    feat: implement tags component with dynamic button rendering from JSON data

--- src/componentes/Galeria/Tags/index.jsx
+++ src/componentes/Galeria/Tags/index.jsx
@@ -1,7 +1,49 @@
-const Tags = () => {
-  return (
-    <p>Tags</p>
-  )
-};
+import tags from "./tags.json"
+import styled from "styled-components"
 
-export default Tags;
+const TituloTags = styled.h2`
+    color: #D9D9D9;
+    font-size: 24px;
+    font-weight: 400;
+`
+
+const TagsContainer = styled.section`
+    display: flex;
+    flex-direction: row;
+    margin-top: 30px;
+    gap: 36px;
+`
+
+const TagsFiltro = styled.div`
+    display: flex;
+    gap: 12px;
+    font-size: 24px;
+    font-weight: 400;
+    button {
+        background: rgba(217, 217, 217, 0.3);
+        border: 2px solid transparent;
+        border-radius: 10px;
+        color: #FFFFFF;
+        margin: 10px;
+        padding: 10px;
+        cursor: pointer;
+        &:hover {
+        border-color: #C98CF1;
+        }
+    }
+`
+
+const Tags = () => {
+    return (
+        <>
+            <TagsContainer>
+                <TituloTags>Busque por tags:</TituloTags>
+                <TagsFiltro>
+                    {tags.map(tag => <button key={tag.id}>{tag.titulo}</button>)}
+                </TagsFiltro>
+            </TagsContainer>
+        </>
+    )
+}
+
+export default Tags
